7a69ab95329c7bd78cca0a68182d73d45550f5be
Taha <taha.ojari@gmail.com>
2023-10-24 10:21:32 +0330

    [feat][nvim] make telescope to search in hidden files and folders

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,4 +1,5 @@
 local telescope = require("telescope")
+local telescopeConfig = require("telescope.config")
 local builtin = require("telescope.builtin")
 
 telescope.load_extension("projects")
@@ -12,5 +13,23 @@ wk.register({
 		g = { builtin.live_grep, "Live grep" },
 		b = { builtin.buffer, "Buffers" },
 		h = { builtin.help_tags, "Help tags" },
+	},
+})
+
+-- TODO: this is deprected, find an alternative
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+table.insert(vimgrep_arguments, "--hidden")
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
+
+telescope.setup({
+	defaults = {
+		vimgrep_arguments = vimgrep_arguments,
+	},
+	pickers = {
+		find_files = {
+			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+		},
 	},
 })
