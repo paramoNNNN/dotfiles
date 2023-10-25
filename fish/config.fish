66b6efd88531cf4273bd872c47dc7ae1ed48c5ea
Taha <taha.ojari@gmail.com>
2023-10-25 11:30:24 +0330

    [feat][fish] add cat alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,9 @@
 
 alias ls "exa --icons"
 alias go "git open"
-alias nano "vim"
 alias docker-rm-all "sudo docker rm -f (sudo docker ps -a -q)"
 alias nvim "nvim --listen /tmp/nvim-server-(tmux display-message -p '#S-#{window_index}').pipe"
-alias vim "nvim"
+alias cat "batcat --theme=base16"
 
 fish_vi_key_bindings
 
