0d9d4efc3ba3612c79b8277393cdc5769b5ac693
Taha <taha.ojari@gmail.com>
2023-11-04 13:04:33 +0330

    [feat][nvim] add fish and sh formatters to conform

--- nvim/after/plugin/conform.lua
+++ nvim/after/plugin/conform.lua
@@ -8,6 +8,8 @@ require("conform").setup({
 		typescript = { { "eslint_d", "prettierd", "prettier" } },
 		typescriptreact = { { "eslint_d", "prettierd", "prettier" } },
 		javascriptreact = { { "eslint_d", "prettierd", "prettier" } },
+		fish = { { "fish_ident" } },
+		sh = { { "shfmt" } },
 	},
 	format_on_save = {
 		-- These options will be passed to conform.format()
