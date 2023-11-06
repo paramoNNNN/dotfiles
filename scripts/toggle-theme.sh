1fe09d03c2b9667137b0a23adfe474ebe67cf246
Taha <taha.ojari@gmail.com>
2023-11-06 11:06:48 +0330

    [feat] get theme name from args when toggling theme

--- scripts/toggle-theme.sh
+++ scripts/toggle-theme.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
+
+MODE=${1}
+
 ALACRITTY_CONFIG_PATH=~/.config/alacritty/alacritty.yml
 KITTY_CONFIG_FOLDER=~/.config/kitty
 TMUX_THEMES_PATH=~/.config/tmux/themes
 LAZYGIT_CONFIG_PATH=~/.config/lazygit/config.yml
-THEME=$(defaults read -g AppleInterfaceStyle 2>/dev/null)
 
-notify() {
-	osascript -e "display notification \"${1}\" with title \"alacritty-theme-toggle\" subtitle \"Switched alacritty theme:\""
+function change_theme {
+    if [ "$1" == "light" ]; then
+        sed -i -e "s/carbonfox/catppuccin-latte/" $ALACRITTY_CONFIG_PATH
+            sed -i -e "s/carbonfox/catppuccin-latte/" "$KITTY_CONFIG_FOLDER/kitty.conf"
+            sed -i -e "s/dark/light/" $LAZYGIT_CONFIG_PATH
+            kitty @ set-colors -a "$KITTY_CONFIG_FOLDER/themes/catppuccin-latte.conf"
+            fish -c "set --universal theme light"
+            tmux source-file "$TMUX_THEMES_PATH/light.conf"
+        if [ "$(uname)" == "Darwin" ]; then
+            osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to false'
+        fi
+    elif [ "$1" == "dark" ]; then
+        sed -i -e "s/catppuccin-latte/carbonfox/" $ALACRITTY_CONFIG_PATH
+            sed -i -e "s/catppuccin-latte/carbonfox/" "$KITTY_CONFIG_FOLDER/kitty.conf"
+            sed -i -e "s/light/dark/" $LAZYGIT_CONFIG_PATH
+            kitty @ set-colors -a "$KITTY_CONFIG_FOLDER/themes/carbonfox.conf"
+            fish -c "set --universal theme dark"
+            tmux source-file "$TMUX_THEMES_PATH/dark.conf"
+        if [ "$(uname)" == "Darwin" ]; then
+            osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to true'
+        fi
+    fi
 }
 
-if [ "$THEME" == "Dark" ]; then
-	sed -i -e "s/carbonfox/catppuccin-latte/" $ALACRITTY_CONFIG_PATH &&
-		sed -i -e "s/carbonfox/catppuccin-latte/" "$KITTY_CONFIG_FOLDER/kitty.conf" &&
-		sed -i -e "s/dark/light/" $LAZYGIT_CONFIG_PATH &&
-		kitty @ set-colors -a "$KITTY_CONFIG_FOLDER/themes/catppuccin-latte.conf" &&
-		fish -c "set --universal theme light" &&
-		tmux source-file "$TMUX_THEMES_PATH/light.conf" &&
-		osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to false' &&
-		notify "light" &&
-		exit
-else
-	sed -i -e "s/catppuccin-latte/carbonfox/" $ALACRITTY_CONFIG_PATH &&
-		sed -i -e "s/catppuccin-latte/carbonfox/" "$KITTY_CONFIG_FOLDER/kitty.conf" &&
-		sed -i -e "s/light/dark/" $LAZYGIT_CONFIG_PATH &&
-		kitty @ set-colors -a "$KITTY_CONFIG_FOLDER/themes/carbonfox.conf" &&
-		fish -c "set --universal theme light" &&
-		fish -c "set --universal theme dark" &&
-		tmux source-file "$TMUX_THEMES_PATH/dark.conf" &&
-		osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to true' &&
-		notify "dark" &&
-		exit
-fi
+change_theme $MODE
+
+exit
