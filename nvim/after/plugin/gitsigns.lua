467e8cb514154517c107423f39c1d9ab23493bfb
Taha <taha.ojari@gmail.com>
2023-11-15 17:19:45 +0330

    [feat][nvim] use whichkey to register gitsigns key maps

--- nvim/after/plugin/gitsigns.lua
+++ nvim/after/plugin/gitsigns.lua
@@ -3,39 +3,60 @@ require("gitsigns").setup({
   current_line_blame_formatter = "<author> • <author_time:%Y-%m-%d> • <summary>",
   signcolumn = true,
   numhl = true,
-  on_attach = function(bufnr)
+  on_attach = function()
     local gs = package.loaded.gitsigns
+    local wk = require("which-key")
 
-    -- TODO: use which-key to register this keymaps
-    local function map(mode, l, r, opts)
-      opts = opts or {}
-      opts.buffer = bufnr
-      vim.keymap.set(mode, l, r, opts)
-    end
-
-    map("n", "]c", function()
-      if vim.wo.diff then
-        return "]c"
-      end
-      vim.schedule(function()
-        gs.next_hunk()
-      end)
-      return "<Ignore>"
-    end, { expr = true })
-
-    map("n", "[c", function()
-      if vim.wo.diff then
-        return "[c"
-      end
-      vim.schedule(function()
-        gs.prev_hunk()
-      end)
-      return "<Ignore>"
-    end, { expr = true })
-
-    map("n", "<leader>hp", gs.preview_hunk)
-    map("n", "<leader>hb", function()
-      gs.blame_line({ full = true })
-    end)
+    wk.register({
+      ["<leader>g"] = {
+        name = "Git",
+        ["]"] = {
+          function()
+            if vim.wo.diff then
+              return "]c"
+            end
+            vim.schedule(function()
+              gs.next_hunk()
+            end)
+            return "<Ignore>"
+          end,
+          "Next hunk",
+          { expr = true },
+        },
+        ["["] = {
+          function()
+            if vim.wo.diff then
+              return "[c"
+            end
+            vim.schedule(function()
+              gs.prev_hunk()
+            end)
+            return "<Ignore>"
+          end,
+          "Previous hunk",
+          { expr = true },
+        },
+        ["v"] = {
+          gs.preview_hunk,
+          "Preview hunk",
+        },
+        ["i"] = {
+          gs.preview_hunk_inline,
+          "Preview hunk inline",
+          { expr = true },
+        },
+        ["b"] = {
+          function()
+            gs.blame_line({ full = true })
+          end,
+          "Blame line",
+        },
+        ["d"] = {
+          gs.diffthis,
+          "Diff this",
+          { expr = true },
+        },
+      },
+    })
   end,
 })
