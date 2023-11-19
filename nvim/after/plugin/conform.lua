bf8ce882a8ebff428c0b4254b76c23031a1fa234
Taha <taha.ojari@gmail.com>
2023-11-19 20:30:13 +0330

    [feat][nvim] use eslint and prettier for formatting json files

--- nvim/after/plugin/conform.lua
+++ nvim/after/plugin/conform.lua
@@ -1,9 +1,8 @@
 require("conform").setup({
   formatters_by_ft = {
     lua = { "stylua" },
-    -- Conform will run multiple formatters sequentially
     python = { "isort", "black" },
-    -- Use a sub-list to run only the first available formatter
+    json = { { "eslint_d", "prettierd", "prettier" } },
     javascript = { { "eslint_d", "prettierd", "prettier" } },
     typescript = { { "eslint_d", "prettierd", "prettier" } },
     typescriptreact = { { "eslint_d", "prettierd", "prettier" } },
@@ -12,7 +11,6 @@ require("conform").setup({
     sh = { { "shfmt" } },
   },
   format_on_save = {
-    -- These options will be passed to conform.format()
     timeout_ms = 500,
     lsp_fallback = true,
   },
