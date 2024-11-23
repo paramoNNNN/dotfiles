235b284ceb7f7da36bb364196bed088872f5edec
Taha <taha.ojari@gmail.com>
2024-11-23 19:18:45 +0330

    [nvim] add key bind to dismiss notifications

--- nvim/after/plugin/nvim-notify.lua
+++ nvim/after/plugin/nvim-notify.lua
@@ -1,2 +1,8 @@
-require("notify").setup({ render = "compact", stages = "fade" })
-vim.notify = require("notify")
+local notify = require("notify")
+notify.setup({ render = "compact", stages = "fade" })
+vim.notify = notify
+
+local wk = require("which-key")
+wk.add({
+  { "<leader><S-C>", "<Cmd>lua require('notify').dismiss()<CR>", desc = "Dismiss notifications" },
+})
