f97a65c4a8705fc9f90ecee215afc37925ade899
Taha <taha.ojari@gmail.com>
2025-03-09 10:28:51 +0330

    update flake lock

--- home/modules/cliphist.nix
+++ home/modules/cliphist.nix
@@ -1,7 +1,7 @@
-{...}: {
+{ ... }: {
   # Install cliphist via home-manager module
   services.cliphist = {
     enable = true;
-    systemdTarget = "hyprland-session.target";
+    systemdTargets = "hyprland-session.target";
   };
 }
