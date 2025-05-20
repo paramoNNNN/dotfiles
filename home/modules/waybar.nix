f3bddc43afe2f80fe431c7787306b6be45641a77
Taha <taha.ojari@gmail.com>
2025-05-20 15:42:41 +0330

    [waybar] minimal theme 2

--- home/modules/waybar.nix
+++ home/modules/waybar.nix
@@ -4,116 +4,153 @@
     settings = {
       mainBar = {
         layer = "top";
-        position = "top";
+        margin-bottom = 0;
+        margin-top = 10;
+        margin-left = 10;
+        margin-right = 10;
+        spacing = 7;
+        height = 42;
 
         modules-left = [ "hyprland/workspaces" ];
+        modules-center = [ "custom/nowplaying" ];
         modules-right =
-          [ "custom/music" "pulseaudio" "backlight" "battery" "clock" "tray" ];
+          [ "network" "pulseaudio" "clock" "custom/notifications" ];
+
+        "custom/notifications" = {
+          tooltip = true;
+          format = "{icon}";
+          format-icons = {
+            notification = "<span foreground='red'><sup></sup></span>";
+            none = "";
+            dnd-notification = "<span foreground='red'><sup></sup></span>";
+            dnd-none = "";
+            inhibited-notification =
+              "<span foreground='red'><sup></sup></span>";
+            inhibited-none = "";
+            dnd-inhibited-notification =
+              "<span foreground='red'><sup></sup></span>";
+            dnd-inhibited-none = "";
+          };
+          return-type = "json";
+          exec-if = "which swaync-client";
+          exec = "swaync-client -swb";
+          on-click = "sleep 0.1 && swaync-client -t -sw";
+          on-click-right = "swaync-client -d -sw";
+          escape = true;
+        };
+
+        "custom/nowplaying" = {
+          return-type = "json";
+          exec = "waybar-mpris --interpolate --order 'ARTIST:TITLE'";
+          on-click = "playerctl play-pause";
+          on-double-click = "playerctl next";
+          on-triple-click = "playerctl previous";
+          escape = true;
+          max-length = 64;
+        };
 
         "hyprland/workspaces" = {
-          "format" = "{icon}";
-          "format-icons" = { "default" = ""; };
+          format = "{icon}";
+          on-click = "activate";
+          format-icons = { "active" = " "; };
+          sort-by-number = true;
+          persistent-workspaces = { "*" = 5; };
         };
 
-        "tray" = {
-          "icon-size" = 21;
-          "spacing" = 10;
+        clock = {
+          format = "{:%a %b %d %H:%M:%S}";
+          format-alt = "{:%Y/%m/%d}";
+          tooltip-format = "{:%a %B %d %H:%M}";
+          interval = 1;
         };
 
-        "custom/music" = {
-          "return-type" = "json";
-          "exec" = "waybar-mpris --interpolate --order 'ARTIST:TITLE'";
-          "on-click" = "waybar-mpris --send toggle";
-          "on-click-right" = "waybar-mpris --send player-next";
-          "escape" = true;
-          "max-length" = 32;
+        network = {
+          tooltip = true;
+          format-wifi = "  {essid}";
+          format-ethernet = "{cidr} ";
+          format-disconnected = "Disconnected ⚠";
         };
-
-        "clock" = {
-          "tooltip-format" = ''
-            <big>{:%Y %B}</big>
-            <tt><small>{calendar}</small></tt>'';
-          "format-alt" = " {:%d/%m/%Y}";
-          "format" = "{:%a %b %d  %H:%M:%S}";
-          "interval" = 1;
-        };
-
-        "pulseaudio" = {
-          "scroll-step" = 5;
-          "format" = "{icon} {volume}%";
-          "format-muted" = "";
-          "format-icons" = { "default" = [ "" "" " " ]; };
-          "on-click" = "pavucontrol";
+        pulseaudio = {
+          format = "{icon}  {volume}%";
+          tooltip = true;
+          format-muted = "󰖁  {format_source}";
+          format-icons = {
+            headphone = "";
+            hands-free = "";
+            headset = "";
+            phone = "";
+            portable = "";
+            car = "";
+            default = [ "" "" "" ];
+          };
+          scroll-step = 5;
+          on-click = "pavucontrol";
         };
       };
     };
 
     style = ''
       * {
-        font-family: FantasqueSansM Nerd Font;
-        font-size: 18px;
-        min-height: 0;
+        font-family: "monospace";
+        font-size: 15px;
       }
 
-      #waybar {
-        background: transparent;
+      window#waybar {
+        background-color: transparent;
+      }
+
+      #window {
+        padding: 0 7px;
         color: @text;
-        margin: 5px 5px;
       }
 
-      #workspaces {
-        border-radius: 1rem;
-        margin: 5px;
-        background-color: @surface0;
-        margin-left: 1rem;
-      }
-
-      #workspaces button {
-        color: @lavender;
-        border-radius: 1rem;
-        padding: 0.4rem;
-        padding-right: 0.7rem;
-      }
-
-      #workspaces button.active {
-        color: @sky;
-        border-radius: 1rem;
-      }
-
-      #workspaces button:hover {
-        color: @sapphire;
-        border-radius: 1rem;
-      }
-
-      #custom-music,
-      #tray,
-      #clock,
       #pulseaudio {
-        background-color: @surface0;
-        padding: 0.5rem 1rem;
-        margin: 5px 0;
+        padding: 0 7px;
+        color: @text;
+      }
+      #pulseaudio.muted {
+        padding: 0 7px;
+        color: @red;
+      }
+
+      #network {
+        padding: 0 7px;
+        color: @text;
+      }
+
+      #custom-nowplaying {
+        padding: 0 7px;
+        padding-right: 0px;
+        color: @text;
+      }
+
+      #custom-notifications {
+        padding: 0 7px;
+        padding-right: 10px;
+        color: @text;
       }
 
       #clock {
+        padding: 0 7px;
+        color: @text;
+      }
+
+      #workspaces button {
+        padding: 0 5px;
+        background-color: transparent;
         color: @blue;
-        border-radius: 0px 1rem 1rem 0px;
-        margin-right: 1rem;
+      }
+      #workspaces button.urgent {
+        background-color: @red;
       }
 
-      #pulseaudio {
-        color: @maroon;
-        border-radius: 1rem 0px 0px 1rem;
-        margin-left: 1rem;
-      }
-
-      #custom-music {
-        color: @mauve;
-        border-radius: 1rem;
-      }
-
-      #tray {
-        margin-right: 1rem;
-        border-radius: 1rem;
+      .modules-right,
+      .modules-left,
+      .modules-center {
+        border: 1px solid @overlay0;
+        border-radius: 15px;
+        background-color: @crust;
+        padding: 0 7px;
       }
     '';
   };
