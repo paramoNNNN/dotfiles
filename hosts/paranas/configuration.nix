567e00b9f390c1af796cd5c6ed8eb135f629b38b
Taha <taha.ojari@gmail.com>
2025-06-22 12:45:58 +0330

    [paranas] set default shell

--- hosts/paranas/configuration.nix
+++ hosts/paranas/configuration.nix
@@ -66,11 +66,13 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  programs.fish.enable = true;
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.paranas = {
     isNormalUser = true;
     description = "paranas";
     extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.fish;
     packages = with pkgs;
       [
         #  thunderbird
