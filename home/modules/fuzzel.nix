aab002d2cf945a9d8852941a16495569ee9c97e8
Taha <taha.ojari@gmail.com>
2025-06-26 10:29:33 +0330

    [fuzzel] add mocha colors

--- home/modules/fuzzel.nix
+++ home/modules/fuzzel.nix
@@ -33,6 +33,19 @@
         selection-match = "209fb5ff";
         counter = "8c8fa1ff";
         border = "209fb5ff";
+
+        # mocha
+        # background = "1e1e2edd";
+        # text = "cdd6f4ff";
+        # prompt = "bac2deff";
+        # placeholder = "7f849cff";
+        # input = "cdd6f4ff";
+        # match = "74c7ecff";
+        # selection = "585b70ff";
+        # selection-text = "cdd6f4ff";
+        # selection-match = "74c7ecff";
+        # counter = "7f849cff";
+        # border = "74c7ecff";
       };
     };
   };
