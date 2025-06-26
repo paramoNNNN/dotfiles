a8de5acd9880754d3016799158be5f2658e898b0
Taha <taha.ojari@gmail.com>
2025-06-26 10:30:16 +0330

    [taha-pc] use router as nameserver

--- hosts/taha-pc/configuration.nix
+++ hosts/taha-pc/configuration.nix
@@ -32,8 +32,7 @@ in {
 
   # Enable networking
   networking.networkmanager.enable = true;
-  networking.nameservers =
-    [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+  networking.nameservers = [ "192.168.1.1" ];
 
   services.resolved = {
     enable = true;
