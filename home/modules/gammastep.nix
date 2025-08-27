e0fd7c4413e81b2479198b4943e3e6d8789a8c20
Taha <paramoNNN@proton.me>
2025-08-27 17:02:50 +0330

    [gammastep] warmer day temp

--- home/modules/gammastep.nix
+++ home/modules/gammastep.nix
@@ -1,7 +1,7 @@
 {
   services.gammastep = {
     enable = true;
-    temperature.day = 6500;
+    temperature.day = 5500;
     temperature.night = 4500;
     dawnTime = "05:00";
     duskTime = "18:00";
