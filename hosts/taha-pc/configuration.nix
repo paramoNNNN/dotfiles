00e3b1cae9f32590e1773f4ce0fe023f69fb4963
Taha <paramoNNN@proton.me>
2026-02-20 18:52:20 +0330

    [taha-pc] docker dns

--- hosts/taha-pc/configuration.nix
+++ hosts/taha-pc/configuration.nix
@@ -178,9 +178,11 @@ in {
 
   virtualisation.docker = {
     enable = true;
+    daemon.settings = { dns = [ "1.1.1.1" "8.8.8.8" ]; };
     rootless = {
       enable = true;
       setSocketVariable = true;
+      daemon.settings = { dns = [ "1.1.1.1" "8.8.8.8" ]; };
     };
   };
 
