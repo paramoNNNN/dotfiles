50404cf697068149335addee071ab434e86bc034
Taha <paramoNNN@proton.me>
2026-02-22 19:07:07 +0330

    [paranas] immich job config

--- hosts/paranas/modules/immich.nix
+++ hosts/paranas/modules/immich.nix
@@ -6,6 +6,44 @@
     openFirewall = true;
     mediaLocation = "/storage/photos";
     settings = {
+      job = {
+        backgroundTask = {
+          concurrency = 1;
+        };
+        faceDetection = {
+          concurrency = 1;
+        };
+        library = {
+          concurrency = 1;
+        };
+        metadataExtraction = {
+          concurrency = 1;
+        };
+        migration = {
+          concurrency = 1;
+        };
+        notifications = {
+          concurrency = 1;
+        };
+        ocr = {
+          concurrency = 1;
+        };
+        search = {
+          concurrency = 1;
+        };
+        sidecar = {
+          concurrency = 1;
+        };
+        smartSearch = {
+          concurrency = 2;
+        };
+        thumbnailGeneration = {
+          concurrency = 1;
+        };
+        videoConversion = {
+          concurrency = 1;
+        };
+      };
       machineLearning = {
         enabled = true;
         urls = [
